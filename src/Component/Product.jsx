ec0d9f4d475c5f00ce1e46759d11c848757231de
Mstchadnee <116938357+Mstchadnee@users.noreply.github.com>
2025-09-05 13:25:39 +0600

    implement responsiveness product page for sm device

--- src/Component/Product.jsx
+++ src/Component/Product.jsx
@@ -40,11 +40,11 @@ const Product = () => {
 //   return <p className="text-center text-xl text-gray-600">Loading...</p>;
 // }
   return (
-    <div className="flex md:flex-row lg:flex-row flex-col h-screen">
+    <div className="flex lg:flex-row flex-col h-screen">
       {/* main flex */}
-      <div className="w-10/12 flex flex-col">
+      <div className=" lg:w-10/12 w-full flex flex-col">
        {/* child col */}
-      <div className="text-xl py-6 flex  justify-start pl-16 gap-16 items-center bg-gradient-to-tr from-[#4b73b0] via-[#77bee1] to-[#26a7ed] shadow-lg">
+      <div className="text-xl py-6 flex  justify-start lg:pl-16 md:pl-16 pl-3 gap-16 items-center bg-gradient-to-tr from-[#4b73b0] via-[#77bee1] to-[#26a7ed] shadow-lg">
              <span className="font-serif uppercase">
                 <span className=""> Agro</span>
                 <span className="text-amber-800">M</span>
@@ -55,13 +55,13 @@ const Product = () => {
               
       </div>
         {/* child col */}
-        <div className=" flex flex-1 h-10/12 gap-5 justify-between items-center bg-[#d3e2ea] pl-16">
-          <div className="space-y-4 pt-10 pb-12">
-            <p className="text-[#d4840e] text-4xl font-serif">{name}</p>
-            <p className="text-amber-800 text-5xl font-bold tracking-tighter font-serif">
+        <div className=" flex lg:flex-row flex-col-reverse flex-1 h-10/12 gap-5 justify-between items-center bg-[#d3e2ea] lg:pl-16">
+          <div className="space-y-4 flex flex-col lg:items-start items-center lg:pt-10 md:pt-7 pt-4 pb-12 px-3 lg:px-0">
+            <p className="text-[#d4840e] md:text-4xl lg:text-4xl text-2xl font-serif lg:text-start text-center">{name}</p>
+            <p className="text-amber-800 md:text-3xl lg:text-5xl text-xl font-bold lg:text-start text-center tracking-tighter font-serif">
               {category}
             </p>
-            <p className="flex items-center justify-start gap-28">
+            <p className="flex items-center lg:justify-start justify-center lg:gap-28 md:gap-28 gap-16">
               <ReactStars
                 count={5}
                 size={24}
@@ -75,21 +75,21 @@ const Product = () => {
                 {quality}
               </span>
             </p>
-            <p className="flex flex-col gap-1 justify-start">
-              <span>
-                <del className="text-stone-500">{oldPrice}</del>
+            <p className="flex flex-col gap-1 lg:justify-start justify-center">
+              <span className="lg:text-start text-center">
+                <del className="text-stone-500 ">{oldPrice}</del>
               </span>
-              <span className="tracking-wider">
+              <span className="tracking-wider lg:text-start text-center">
                 <span className="text-amber-800 tracking-wider font-bold pr-1">
                   {price}
                 </span>
                 BDT Only !!!
               </span>
             </p>
-            <p className=" font-thin tracking-wider text-stone-500 font-serif">
+            <p className=" font-thin tracking-wider text-justify text-stone-500 font-serif lg:text-start ">
               {description}
             </p>
-            <p className="pt-12">
+            <p className="lg:pt-12 mg:pt-8 pt-5 lg:text-start text-center">
               {/* <button className="bg-amber-800 text-white px-5 py-2 rounded-md">
                 Purchase First !
               </button> */}
@@ -116,11 +116,12 @@ const Product = () => {
 />
             </p>
           </div>
-          <img src={image} className=" w-2/5 h-[420px] pr-12" alt="" />
+          <img src={image} className="lg:w-2/5 md:w-full h-[420px] lg:pr-12 lg:px-0 px-3 lg:pt-0 pt-5" alt="" />
         </div>
 
-        {/* child col */}
-        <div className="text-sm bg-gradient-to-tr from-[#4b73b0] via-[#77bee1] to-[#26a7ed] shadow-lg flex flex-1 justify-between items-center px-16">
+        {/* child col - hidden for small device*/}
+        <div className="lg:block md:block hidden ">
+            <div className="text-sm py-3 bg-gradient-to-tr from-[#4b73b0] via-[#77bee1] to-[#26a7ed] shadow-lg flex flex-1 justify-between items-center px-16">
           <div className="text-[#1e4f68] tracking-tighter flex flex-col justify-center items-center">
             <p>Trusted by over 3,50,000 clients</p>
             <p>Worldwide since 2008</p>
@@ -156,25 +157,66 @@ const Product = () => {
           </div>
          </div>
         </div>
+        </div>
+        
 
       </div>
       {/* main flex */}
-      <div className="w-2/12 bg-gradient-to-tr from-[#4b73b0] via-[#77bee1] to-[#26a7ed] shadow-lg h-full flex flex-col items-center pl-4 pr-4">
+      <div className="lg:w-2/12 w-full bg-sky-600 lg:bg-gradient-to-tr from-[#4b73b0] via-[#77bee1] to-[#26a7ed] shadow-lg lg:h-full flex flex-col items-center pl-4 pr-4">
         <p className="py-10 text-2xl font-thin text-white ">You May Like</p>
-     <div className="flex flex-col space-y-5 gap-4 items-center justify-evenly">
+     <div className="flex flex-col space-y-5 gap-4 items-center justify-evenly cursor-pointer">
           {product
     .sort(() => Math.random() - 0.5) // shuffle randomly
     .slice(0, 3) // take only 3 after shuffle
     .map((item) => (
       <Link to={`/products/${item._id}`} key={item._id}>
         <div className="text-white text-center">
-          <img src={item.image} className="h-[130px] w-full" alt="" />
-          <p className="font-thin">{item.category}</p>
+          <img src={item.image} className="lg:h-[130px] h-[200px]  w-full" alt="" />
+          <p className="font-thin lg:text-[16px] text-xl">{item.category}</p>
         </div>
       </Link>
     ))}
         </div>
       </div>
+
+       {/* child col- only for small device*/}
+        <div className="lg:hidden md:hidden block ">
+            <div className="text-sm bg-gradient-to-tr from-[#2d76ca] via-[#5ba0ee] to-[#26a7ed] shadow-lg flex flex-col justify-between items-center gap-24 py-12 px-3">
+          <div className="text-white tracking-tighter flex flex-col justify-center items-center">
+            <p>Trusted by over 3,50,000 clients</p>
+            <p>Worldwide since 2008</p>
+          </div>
+               <div className="text-white font-semibold flex flex-col justify-center items-center">
+            <p className="text-4xl">{ratings}</p>
+            <p>
+              {" "}
+              <ReactStars
+                count={5}
+                size={24}
+                activeColor="#ffd700"
+                value={ratings}
+                inactiveFillColor="#2c3973"
+                isHalf={true} // ✅ allows half stars
+                edit={false}
+              />{" "}
+            </p>
+            <p>Positive customer review</p>
+          </div>
+          <div className="text-white font-semibold flex flex-col justify-center items-center">
+            <p className="text-4xl">6M+</p>
+            <p className="">Best Selling Products</p>
+            <p className="">
+              All rights are reserved by
+              <span className="font-serif uppercase">
+                <span className=""> Agro</span>
+                <span className="text-amber-800">M</span>
+                <span className="">Art</span>
+              </span>
+            </p>
+          </div>
+         
+        </div>
+        </div>
     </div>
   );
 };
